bca16c9d0deca81ab4fa7075aa63c92889c1d26a
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-10-16 14:54:37 +0200

    update config...

--- config/mysql/01_configure.sql
+++ config/mysql/01_configure.sql
@@ -22,8 +22,20 @@ UPDATE guacamole_user SET
     password_date = CURRENT_TIMESTAMP WHERE entity_id = 1;
 
 -- Create connection
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Windows', 'rdp');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Windows Server (ad)', 'rdp');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Database Server (db)', 'ssh');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('OUD Server (oud)', 'ssh');
 
--- Add parameters to the new connection
-INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', 'ad');
+-- Add parameters to the Windows connection
+INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '10.0.1.4');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'port', '3389');
+-- Add parameters to the Database connection
+INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', '10.0.1.6');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'port', '22');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'username', 'oracle');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'private-key', 'PRIVATEKEY');
+-- Add parameters to the OUD connection
+INSERT INTO guacamole_connection_parameter VALUES (3, 'hostname', '10.0.1.5');
+INSERT INTO guacamole_connection_parameter VALUES (3, 'port', '22');
+INSERT INTO guacamole_connection_parameter VALUES (3, 'username', 'oracle');
+INSERT INTO guacamole_connection_parameter VALUES (3, 'private-key', 'PRIVATEKEY');
