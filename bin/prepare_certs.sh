2655414b63395e7539d7a2a905100d0feb72dc09
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-10-26 12:23:55 +0100

    update script

--- bin/prepare_certs.sh
+++ bin/prepare_certs.sh
@@ -48,43 +48,43 @@ if ! command_exists docker-compose; then
     exit 1
 fi
 
-
-# if [ -d "$data_path" ]; then
-#   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#     exit
-#   fi
-# fi
-
+echo "INFO: Start to config certs at $(date)" 
 # change workding directory
 cd ${SCRIPT_BASE}
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
+
+# check if we do have data
+if [ ! -d "$data_path" ]; then
+  if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+    echo "INFO: Downloading recommended TLS parameters ..." 
+    mkdir -p "$data_path/conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+    echo
+  fi
+
+  echo "INFO: Creating dummy certificate for $domains ..." 
+  path="/etc/letsencrypt/live/$domains"
+  mkdir -p "$data_path/conf/live/$domains"
+  docker-compose run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+
+  echo "INFO: Starting nginx ..." 
+  docker-compose up --force-recreate -d nginx
+
+  echo "INFO: Deleting dummy certificate for $domains ..." 
+
+  docker-compose run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domains && \
+    rm -Rf /etc/letsencrypt/archive/$domains && \
+    rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+else
+  echo "INFO: Existing data found for $domains ..." 
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
+echo "INFO: Requesting Let's Encrypt certificate for $domains ..." 
 #Join $domains to -d args
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
@@ -107,7 +107,9 @@ docker-compose run --rm --entrypoint "\
     --rsa-key-size $rsa_key_size \
     --agree-tos --no-eff-email \
     --force-renewal" certbot
-
-echo "### Reloading nginx ..."
-docker-compose ps
+    
+echo "INFO: Reloading nginx ..." 
 docker-compose exec nginx nginx -s reload
+
+echo "INFO: Finish to config certs at $(date)"
+# --- EOF --------------------------------------------------------------------
