2dcfc98b9428d8e67941876fb064b96a218cf3c0
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-10-26 19:57:53 +0100

    change to docker restart

--- bin/prepare_certs.sh
+++ bin/prepare_certs.sh
@@ -109,7 +109,7 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
     
 echo "INFO: Reloading nginx ..." 
-docker-compose exec nginx nginx -s reload
+docker restart nginx
 
 echo "INFO: Finish to config certs at $(date)"
 # --- EOF --------------------------------------------------------------------
