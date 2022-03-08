82e3f3e9873568819a82c33b9337dfa05cabb7ee
Stefan Oehrli <stefan.oehrli@trivadis.com>
2022-03-08 22:44:04 +0100

    fix minor

--- bin/setup_guacamole.sh
+++ bin/setup_guacamole.sh
@@ -143,13 +143,11 @@ docker-compose up -d guacamole mysql guacd
 ${GUACAMOLE_BASE}/bin/prepare_certs.sh
 
 # prepare openvpn 
-
-HOSTNAME=${HOSTNAME:-$(hostname)}                # Hostname for the bastion host
-export DOMAINNAME
+echo "INFO: Configure OpenVPN -----------------------------------------------"
 docker-compose run --rm openvpn ovpn_genconfig -u "udp://${HOSTNAME}.${DOMAINNAME}" -p "push route 10.0.1.0 255.255.255.0"
 echo "${HOSTNAME}.${DOMAINNAME}"|docker-compose run --rm openvpn ovpn_initpki nopass
-docker-compose run --rm openvpn easyrsa build-client-full $(hostname) nopass
-docker-compose run --rm openvpn ovpn_getclient >$SCRIPT_BASE/${HOSTNAME}.ovpn
+docker-compose run --rm openvpn easyrsa build-client-full ${HOSTNAME} nopass
+docker-compose run --rm openvpn ovpn_getclient ${HOSTNAME} >$SCRIPT_BASE/${HOSTNAME}.ovpn
 echo "pull" >>$SCRIPT_BASE/${HOSTNAME}.ovpn
 echo "tls-client" >>$SCRIPT_BASE/${HOSTNAME}.ovpn
 echo "remote ${HOSTNAME}.${DOMAINNAME}">>$SCRIPT_BASE/${HOSTNAME}.ovpn
