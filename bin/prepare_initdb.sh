cb4c72deeb2549e549a36afe8236810b38c73860
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-03-14 09:28:54 +0100

    update file headers

--- bin/prepare_initdb.sh
+++ bin/prepare_initdb.sh
@@ -4,7 +4,7 @@
 # Saegereistrasse 29, 8152 Glattbrugg, Switzerland
 # ---------------------------------------------------------------------------
 # Name.......: bootstrap_bastion.sh 
-# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@accenture.com
 # Editor.....: Stefan Oehrli
 # Date.......: 2020.04.28
 # Revision...: 
--- config/mysql/01_configure.sql
+++ config/mysql/01_configure.sql
@@ -3,7 +3,7 @@
 -- Saegereistrasse 29, 8152 Glattbrugg, Switzerland
 -- -------------------------------------------------------------------------
 -- Name.......: 01_configure.sql
--- Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+-- Author.....: Stefan Oehrli (oes) stefan.oehrli@accenture.com
 -- Editor.....: Stefan Oehrli
 -- Date.......: 2020.10.26
 -- Revision...: 
